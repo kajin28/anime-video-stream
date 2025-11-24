2549559272ac268049e4d5864e5348f43c935bd1
kajin28 <kajinkaja9@gmail.com>
2025-11-24 18:37:58 +0530

    vercel config

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from.import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home,name='home'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
@@ -12,3 +16,6 @@ urlpatterns = [
     path('payment/<int:plan_id>/', views.payment_view, name='payment'),
     path('payment/confirm/', views.payment_confirm, name='payment_confirm'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- myproject/settings.py
+++ myproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kbjf!tf4#cv6cycelhz+cl7+&h2%gn*-gm&4!^dj3z1da6bnaw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '.now.sh']
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
